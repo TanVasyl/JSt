e673768589cf7322dee9954cb9deb726313f0ce2
TanVasyl <99660402+TanVasyl@users.noreply.github.com>
2022-03-24 14:02:31 +0300

    opt

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 'use strict';
 
 let number = []
-let buttonAdd = document.querySelector(".b1");
+
 let buttonClear = document.querySelector(".clear-button");
-    function sumAndMul () {
-          let val = document.querySelector(".in1").value
-        number.push(+val)
-      console.log (val);
-          let sum = number.reduce((sum, current) => sum + current, 0)
-      document.querySelector(".out").innerHTML = (`Сумма ваших чисел равна: ${sum}`);
-          let mul = number.reduce((mul, current) => mul * current, 1)
-      document.querySelector(".out1").innerHTML = (`Произведение ваших чисел равна: ${mul}`);
-    }
-    function del () {
-        number = []
-        document.querySelector(".out").innerHTML = (`Сумма ваших чисел равна: `);
-        document.querySelector(".out1").innerHTML = (`Произведение ваших чисел равна: `);
-    }
+let buttonAdd = document.querySelector(".b1")
 document.addEventListener ('keyup', function(event) {
-    if (event.code === 'Enter') {
-        return sumAndMul();
-    } else {
-   (event.code === 'NumpadEnter') 
-        return sumAndMul();
-    }
+    if (event.code === 'Enter' || event.code === 'NumpadEnter' || event.code === 'mouseup' )  {
+            let val = document.querySelector(".in1").value;
+                 number.push(+val);
+                
+            let sum = number.reduce((sum, current) => sum + current, 0)
+            let mul = number.reduce((mul, current) => mul * current, 1)
+            document.querySelector(".out").innerHTML = (`Сумма ваших чисел равна: ${sum} </br>
+        Произведение ваших чисел равна: ${mul}`);
+        }
 });
 document.addEventListener ('keyup', function(event) {
     if( event.code === 'Delete') {
     return del();
+    }
+    function del () {
+        number = []
+        document.querySelector(".out").innerHTML = (`Результат сброшен! `);
     }
 })
