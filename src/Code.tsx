236c72dca270a395347c348befdcc228abb06666
TanVasyl <99660402+TanVasyl@users.noreply.github.com>
2022-05-19 17:32:14 +0300

    Update Code.tsx

--- src/Code.tsx
+++ src/Code.tsx
@@ -27,45 +27,58 @@ class Code extends React.Component<{}, CodeInputState>{
       number:[...this.state.number, +this.state.title]
     })   
     console.log(this.state.number)
+   
     
   }
-   
+  deleteResult = () => {
+    this.setState({
+      number: []
+    })
+  }
+
+    
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <button onClick={this.showArray}>Calculation?</button>
+          <button onClick={this.showArray}>Считаем?</button>
         <input type ="number" className = "input-add" placeholder="Write number" onInput={this.input}/>
+        <button onClick={this.deleteResult}>Обнуляем?</button>
+       
         </header>
-          <h3>{`Результат сложения ваших чисел равен: ${this.state.number.reduce((sum:number, current:number) => sum + current, 0)}`}</h3>
-          <h3>{`Результат умножения ваших чисел равен: ${this.state.number.reduce((mul:number, current:number) => mul * current, 1)}`}</h3>
+        <ul>Введеные чи
+          {this.state.number.map(elem => {
+            return <li>{elem}</li>
+          })}
+        </ul>
+        <ul>Произведения чисел
+          {
+          this.state.number.map((elem,index,arr) => {
+            return (
+            <li key={elem.toString()}>
+              {
+            arr.reduce((prev, curr) => prev + curr, 0)
+            }</li>
+            )})}
+        </ul>       
+      
+        <ul>Произведения чисел
+          {
+          this.state.number.map((elem,index,arr) => {
+            return (
+            <li key={elem.toString()}>
+              {
+            arr.reduce((prev, curr) => prev * curr, 1)
+            }</li>
+            )})}
+        </ul> 
       </div>
       );
   }  
 }
 
+  
+
+
 export default Code;
-
-// let number: Array<number> = []
-
-// let buttonClear = document.querySelector(".clear-button");
-// let buttonAdd = document.querySelector(".b1")
-// document.addEventListener ('keyup', function(event):void {
-//     if (event.code === 'Enter' || event.code === 'NumpadEnter')  {
-//             let val:string =(<HTMLInputElement> document.querySelector(".in1")).value;
-//                  number.push(+val);
-                
-//             let sum = number.reduce((sum, current) => sum + current, 0)
-//             let mul = number.reduce((mul, current) => mul * current, 1)
-//             document.querySelector(".out").innerHTML = `Результат сложения ${sum}</br> Результат умножения ${mul}` 
-//         }
-// });
-// document.addEventListener ('keyup', function(event):void {
-//     if( event.code === 'Delete') {
-//     return del();
-//     }
-//     function del () {
-//         number = []
-//         document.querySelector(".out").innerHTML = `Результат сброшен!`
-//      }
-// })
