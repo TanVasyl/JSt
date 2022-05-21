149669830e8d94bd2d74ca650eb8851445beebc4
TanVasyl <99660402+TanVasyl@users.noreply.github.com>
2022-05-21 16:50:09 +0300

    Update Code.tsx

--- src/Code.tsx
+++ src/Code.tsx
@@ -4,80 +4,80 @@ import { Argument } from 'webpack';
 
 
 interface CodeInputState {
-  title ?: string;
+  title ?: any;
   number?: Array<number>;
-  
+  sum?:Array<number>;
+  mul?:Array<number>;
 }
-
-let array:any = [];
-
 
 class Code extends React.Component<{}, CodeInputState>{
   constructor(props:string) {
     super(props);
     this.state = {
-      title: ``,
-      number: []
+      title: '',
+      number: [],
+      sum: [],
+      mul: []
       }
   }
-  showText = (event:any) => {
-    let a = event.target.value;
-    this.setState({
-      title: a,
-      number: array
-    });
-  }
-  showArray = () => {
-    this.setState({
-      number: array.push(+this.state.title)
+  input = (event:any) => {
+    this.setState ({
+      title: event.target.value
     })
-     
-    
-    console.log(this.state.number)
-    console.log(array)
   }
+  showArray = (input:any) => {
+    this.setState({
+      number:[...this.state.number, +this.state.title],
+      sum: [...this.state.sum, this.state.number.reduce((prev,curr) => prev+curr, +this.state.title)],
+      mul: [...this.state.sum, this.state.number.reduce((prev,curr) => prev*curr, +this.state.title)]
+      })   
+    
+    // console.log(this.state.number)
+    console.log(this.state.sum)
    
-  render() {
+    
+  }
+  deleteResult = () => {
+    this.setState({
+      number: []
+    })
+  }
+
+
   
-    let sum:number = array.reduce((sum:number, current:number) => sum + current, 0)
-    let mul:number = array.reduce((mul:number, current:number) => mul * current, 1)
+  render() {
+   const ulStyle = {
+     
+   }
     return (
       <div className="App">
         <header className="App-header">
-          <button onClick={this.showArray}>Calculation?</button>
-        <input type ="number" className = "input-add" placeholder="Write number" onInput={this.showText}/>
+          <button onClick={this.showArray}>Считаем?</button>
+        <input type ="number" className = "input-add" placeholder="Write number" onInput={this.input}/>
+        <button onClick={this.deleteResult}>Обнуляем?</button>
+       
         </header>
-          <h3>{`Результат сложения ваших чисел равен: ${sum}`}</h3>
-          <h3>{`Результат умножения ваших чисел равен: ${mul}`}</h3>
+        <ul style={{display:'inline-block'}}>Введеные числа
+          {this.state.number.map(elem => {
+            return <li>{elem}</li>
+          })}
+        </ul>
+        <ul style={{display:'inline-block'}}>Сумма чисел
+        {this.state.sum.map((elem,ind,arr) => {
+            return <li>{arr[ind]}</li>
+          })}
+        </ul>       
+        <ul style={{display:'inline-block'}}>Произведение чисел
+        {this.state.mul.map((elem,ind,arr) => {
+            return <li>{arr[ind]}</li>
+          })}
+        </ul>      
       </div>
       );
-
   }  
 }
 
+  
+
 
 export default Code;
-
-// let number: Array<number> = []
-
-// let buttonClear = document.querySelector(".clear-button");
-// let buttonAdd = document.querySelector(".b1")
-// document.addEventListener ('keyup', function(event):void {
-//     if (event.code === 'Enter' || event.code === 'NumpadEnter')  {
-//             let val:string =(<HTMLInputElement> document.querySelector(".in1")).value;
-//                  number.push(+val);
-                
-//             let sum = number.reduce((sum, current) => sum + current, 0)
-//             let mul = number.reduce((mul, current) => mul * current, 1)
-//             document.querySelector(".out").innerHTML = `Результат сложения ${sum}</br> Результат умножения ${mul}` 
-//         }
-// });
-// document.addEventListener ('keyup', function(event):void {
-//     if( event.code === 'Delete') {
-//     return del();
-//     }
-//     function del () {
-//         number = []
-//         document.querySelector(".out").innerHTML = `Результат сброшен!`
-//      }
-// })
