744e4ac5293c23cf1b364472362027b8640089d7
TanVasyl <99660402+TanVasyl@users.noreply.github.com>
2022-05-24 13:47:10 +0300

    Update Code.tsx

--- src/Code.tsx
+++ src/Code.tsx
@@ -6,7 +6,7 @@ import { Argument} from 'webpack';
 interface CodeInputState {
   title ?: any;
   number?: Array<number>;
-  sum?:Array<number>;
+  sum?:Array<number>  ;
   mul?:Array<number>;
 }
 
@@ -25,30 +25,35 @@ class Code extends React.Component<{}, CodeInputState>{
       title: event.target.value
     })
   }
-  showArray = (input:any) => {
+  showArray = () => {
     this.setState({
-      number:[...this.state.number, +this.state.title],
-      sum: [...this.state.sum, this.state.number.reduce((prev,curr) => prev+curr, +this.state.title)],
-      mul: [...this.state.sum, this.state.number.reduce((prev,curr) => prev*curr, +this.state.title)]
+      number: this.state.number.concat(+this.state.title), 
+      sum: this.state.number.concat(this.state.number.reduce((prev,curr)=> prev+curr, +this.state.title)),
+      mul: this.state.number.concat(this.state.number.reduce((prev,curr)=> prev*curr, +this.state.title)),
       })   
-    
-    // console.log(this.state.number)
-    console.log(this.state.sum)
    
-    
+    console.log(this.state.sum)
   }
   deleteResult = () => {
     this.setState({
-      number: []
+      number: [],
+      sum: [],
+      mul: []
     })
   }
-
+  deleteStr = () => {
+    this.setState({
+      number:this.state.number.slice(1),
+      // sum:  [...this.state.sum, this.state.number.slice(1).reduce((prev,curr) => prev+curr, +this.state.title)]
+    })
+    // console.log(this.state.number)
+    console.log(this.state.sum)
+  }
+    
+  
 
   
   render() {
-   const ulStyle = {
-     
-   }
     return (
       <div className="App">
         <header className="App-header">
@@ -64,14 +69,19 @@ class Code extends React.Component<{}, CodeInputState>{
         </ul>
         <ul style={{display:'inline-block'}}>Сумма чисел
         {this.state.sum.map((elem,ind,arr) => {
-            return <li>{arr[ind]}</li>
+            return <li>{this.state.number.reduce((prev,curr) => prev+curr,0)}</li>
           })}
         </ul>       
         <ul style={{display:'inline-block'}}>Произведение чисел
         {this.state.mul.map((elem,ind,arr) => {
-            return <li>{arr[ind]}</li>
+            return <li>{this.state.number.reduce((prev,curr) =>  prev*curr,1)}</li>
           })}
-        </ul>      
+        </ul>
+        <ul style={{display:'inline-block'}}> Тыкай тут, что бы удалить строку
+         {this.state.number.map( () => {
+           return <li><button onClick={this.deleteStr}>Удали Меня</button></li>
+         })}
+        </ul>    
       </div>
       );
   }  
